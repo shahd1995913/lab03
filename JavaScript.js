a261f5d89a60319dd28b5f6f3f3097b77464e9d4
shahdAlkhatib <>
2021-06-03 12:32:27 +0000

    updae2021/3/6

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,30 @@
-alert("Wellcom to my page")
+alert ( "welecom to my page , please inter the programimg languege that prefer" )
+
+var answer = prompt ("What the type of programming languge that prefer")
+
+if(answer == "python"){alert ("ok.")}
+
+else if(answer == "java"){alert ("great we use java to build a websits.")}
+
+else if(answer == "C#"){alert ("Sorry we do not support this languge")}
+else if(answer == "C++"){alert ("ok.")}
+
+else if(answer == "R"){alert ("greet this is the best program that support the mechine learning.")}
+
+else if(answer == "javascript"){alert ("ok.")}
+
+else if(answer == "orange"){alert ("ok.")}
+
+var input =prompt('how many course intrested to learn , please insert number')
+
+while (input != 5)
+{
+input =prompt('sorry we do not have this number try again pleae!!')
+}
+var val =prompt('how many image for programimg Languages you want to see')
+for(i=0;i<val;i++)
+{
+  document.write("<img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/Best-Programming-Languages-to-Start-Learning-Today.jpg' ,width='200px', hight='300';>")
+}
 
 
